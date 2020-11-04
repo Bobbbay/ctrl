97543375445d407b334cba22f92a17f6a5d78f2c
Bobbbay <abatterysingle@gmail.com>
2020-11-04 01:06:17 -0500

    Complete code!

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  const { exec } = require("child_process");
+  const { spawn } = require("child_process");
 
   app.on("issues.opened", async (context) => {
     app.log.info(context);
@@ -33,23 +33,69 @@ module.exports = (app) => {
     let link = `https://github.com/${branch[0]}/${repo}/tree/${branch[1]}`;
     let toClone = link.split("/tree/");
 
+    const command = spawn("./build-sh", [toClone[0], toClone[1]]);
+    let logs = "";
+
+    command.stdout.on("data", (data) => {
+      logs += data;
+    });
+
+    command.stderr.on("data", (data) => {
+      logs += data;
+    });
+
+    command.on("error", (error) => {
+      logs += data;
+    });
+
+    command.on("close", (code) => {
+      logs += `process exited with code ${code}`;
+      console.log(logs);
+
+      if (!code) {
+        const issueComment = context.issue({
+          body: `Thank you for submitting your PR. It seems like this PR passes all checks. You can find the logfile attached below.`,
+        });
+        return context.github.issues.createComment(issueComment);
+      } else {
+        const issueComment = context.issue({
+          body: `Thank you for submitting your PR. Sadly, it seems like some build(s) failed. The log file is attached below.
+          <details>
+<summary>Logs</summary>
+<p>
+
+\`\`\`
+${logs}
+\`\`\`
+
+</p>
+</details>  
+          `,
+        });
+        return context.github.issues.createComment(issueComment);
+      }
+    });
+
+    /*
+
     exec(`./build-sh ${toClone[0]} ${toClone[1]}`, (error, stdout, stderr) => {
       if (error) {
+        const issueComment = context.issue({
+          body: `Thank you for submitting your PR. Sadly, it seems like the build has failed a check. Here are the error codes for details: ${error.message}.`,
+        });
         console.log(`error: ${error.message}`);
-        return;
+        return context.github.issues.createComment(issueComment);
       }
       if (stderr) {
         console.log(`stderr: ${stderr}`);
         return;
       }
       console.log(`stdout: ${stdout}`);
-    });
-
-    const issueComment = context.issue({
-      body: `Thank you for opening PR ${context["number"]}.`,
-    });
-
-    return context.github.issues.createComment(issueComment);
+      const issueComment = context.issue({
+        body: `Thank you for submitting your PR. It seems like this PR passes all checks.`,
+      });
+      return context.github.issues.createComment(issueComment);
+    });*/
   });
   // For more information on building apps:
   // https://probot.github.io/docs/
